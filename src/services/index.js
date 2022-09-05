60ebef5acebf7efa0758b8029e17894933cb43fc
OTTO886 <54676386+OTTO886@users.noreply.github.com>
2022-09-05 16:27:53 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -89,7 +89,7 @@ import dayjs from 'dayjs'
     }).catch(err => err)
 
     if (res.status === 200 && res) {
-        print(res.data)
+        console.log(res.data)
         return res.data
     }
     console.error('发生错误', res)
